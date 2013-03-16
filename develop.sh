382963f62e52c52f9a39f426cf96fd8055070867
Adam Whittingham <adam.whittingham@gmail.com>
2013-03-16 23:38:59 +0000

    Rejig develop.sh to launch chrome and gvim in better places

--- develop.sh
+++ develop.sh
@@ -9,19 +9,25 @@ pushd $project_root >/dev/null
 
 tmux has-session -t $tmux_session
 if [ $? != 0 ]; then
+  export TERM=xterm-256color # Find a less janky way to fix this
+
   echo "Setting up new tmux session '$tmux_session'"
+  tmux new-session -s $tmux_session -n tests -d
 
-  tmux new-session -s $tmux_session -n monitoring -d
-
+  # Create window 1: Tests & git status
   tmux send-keys -t ${tmux_session}:1 'guard' C-m
   tmux split-window -t $tmux_session -p 25 -h
+  tmux send-keys -t ${tmux_session}:1 'watch -c -n 5 -t git status -sb' C-m
 
-  tmux send-keys -t ${tmux_session}:1.2 'bundle exec rackup -p 4567' C-m
+  # Create window 2: Web server logs
+  tmux new-window -t $tmux_session -n webserver
+  tmux send-keys -t ${tmux_session}:2 'bundle exec rackup -p 4567' C-m
 
+  # Create Window 3: shell for playing in
   tmux new-window -t $tmux_session -n shell
-  tmux split-window -t ${tmux_session}:2 -h -p 20
-  tmux send-keys -t ${tmux_session}:2.1 'gvim' C-m
-  tmux send-keys -t ${tmux_session}:2.2 'watch -c -n 5 -t git status -sb' C-m
+  tmux send-keys -t ${tmux_session}:3 '(sleep 5 && chromium-browser localhost:4567) &' C-m
+  tmux send-keys -t ${tmux_session}:3 'gvim -geometry 500x500' C-m
+  tmux send-keys -t ${tmux_session}:3 'clear; ls -l' C-m
 
   tmux select-window -t ${tmux_session}:1
 fi
